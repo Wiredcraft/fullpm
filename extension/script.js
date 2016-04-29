8985042745bde1f6984d6de60c0f73955aebd2d0
Juha Suomalainen <juha@wiredcraft.com>
2016-04-29 18:47:37 +0800

    Improve extension: load iframe, show kanban button.

--- extension/script.js
+++ extension/script.js
@@ -1,22 +1,24 @@
 console.log('loaded')
 console.log(location.href);
 
-setTimeout(function(){
-
-
 if (location.href.startsWith('https://github.com/Wiredcraft/data.worldbank.org/issues')) {
-  els = window.document.getElementsByClassName('subnav-links')
+  els = window.document.getElementsByClassName('js-repo-nav')
 
   if (els){
     buttonListEl = els[0];
     aEl = document.createElement('a');
     aEl.addEventListener('click', function(){
-      alert('Show Kanban')
+      var iframe = document.createElement('iframe');
+      iframe.src = chrome.runtime.getURL('frame.html');
+      iframe.style.cssText = 'background-color:pink; position:fixed;top:0;left:0;display:block;' +
+                             'width:300px;height:100%;z-index:1000;';
+      buttonListEl.appendChild(iframe);
     })
     aEl.text = 'Kanban';
-    aEl.classList = ['js-selected-navigation-item', 'subnav-item']
+    aEl.href = '#test';
+    aEl.classList = ['js-selected-navigation-item reponav-item']
     buttonListEl.appendChild(aEl)
+
+
   }
 }
-
-}, 2000)
