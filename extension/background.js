0411047ebccdf2f5f5e2856372417cf810e46394
Jamesford <jamiestobbs92@gmail.com>
2016-05-13 15:42:21 +0200

    Clean up extension contentscript

--- extension/background.js
+++ extension/background.js
@@ -3,7 +3,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete') {
     chrome.tabs.sendMessage(tabId, { type: changeInfo.status }, function(response) {
       if (!response) return false
-      console.log(response)
+      console.info('Unexpected message response:', response)
     })
   }
 })
--- extension/script.js
+++ extension/script.js
@@ -1,44 +1,43 @@
-console.log('loaded')
-console.log(location.href);
+console.info('pmhub running...', location.href)
 
 // Append kanban button to page
 function appendButtonToNav () {
   // Stop if button exists already
   if (document.getElementById('pmhub')) { return null }
 
-  navElem = document.getElementsByClassName('js-repo-nav')
+  navElem = document.getElementsByClassName('js-repo-nav')[0]
   if (navElem) {
-    buttonListEl = navElem[0];
     aEl = document.createElement('a');
-    aEl.addEventListener('click', function(){
-      issuesEl = document.getElementsByClassName('repository-content')[0]
-      issuesEl.style.display = 'none';
-
-      var containerEl = document.getElementsByClassName('container experiment-repo-nav')[0]
-
-      var iframe = document.createElement('iframe');
-      iframe.src = chrome.runtime.getURL('frame.html');
-      iframe.style.cssText = 'width:100%;height:100%;border:none';
-      //buttonListEl.appendChild(iframe);
-
-      var divEl = document.createElement('div');
-      divEl.classList = ['repository-content']
-      divEl.appendChild(iframe)
-
-      containerEl.appendChild(divEl)
-
-      var activeLinkEl = document.getElementsByClassName('selected reponav-item')[0]
-      activeLinkEl.classList.remove("selected")
-
-      aEl.classList.add('selected')
-    })
-
+    aEl.addEventListener('click', appendContentToPage)
     aEl.text = 'Kanban';
     aEl.id = 'pmhub';
     aEl.href = '#pmhub';
     aEl.classList = ['js-selected-navigation-item reponav-item']
-    buttonListEl.appendChild(aEl)
+    navElem.appendChild(aEl)
   }
+}
+
+function appendContentToPage () {
+  // Tab content element
+  contentElem = document.getElementsByClassName('repository-content')[0]
+
+  // Create our content
+  var iframe = document.createElement('iframe');
+  iframe.src = chrome.runtime.getURL('frame.html');
+  iframe.style.cssText = 'width:100%;height:100%;border:none';
+
+  // Clear contentElem for our content
+  while (contentElem.firstChild) {
+    contentElem.removeChild(contentElem.firstChild);
+  }
+
+  // Append our content to the content element
+  contentElem.appendChild(iframe)
+
+  // Our link should be selected now
+  var activeLinkEl = document.getElementsByClassName('selected reponav-item')[0]
+  activeLinkEl.classList.remove("selected")
+  this.classList.add('selected')
 }
 
 // Background script message listener
@@ -48,7 +47,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       appendButtonToNav()
       break
     default:
-      console.log('Message received:', request, sender)
+      console.info('Unexpected message received:', request, sender)
       break
   }
 })
