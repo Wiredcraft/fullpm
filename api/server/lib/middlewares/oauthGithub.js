c891c878253ce8700e3a8e0fda45a9a837252fae
Makara Wang <makara15@gmail.com>
2016-07-22 17:38:46 +0800

    Default to repo scope.

--- api/server/lib/middlewares/oauthGithub.js
+++ api/server/lib/middlewares/oauthGithub.js
@@ -1,29 +1,39 @@
 'use strict';
 
-// var lib = require('../');
+const debug = require('debug')('kenhq:middleware:oauthGithub');
 
-// var _ = require('lodash');
+// const lib = require('../');
 
-var passport = require('passport');
+// const _ = require('lodash');
+
+const passport = require('passport');
 
 module.exports = function(options) {
   return function oauthGithub(req) {
     // Handle OAuth callback.
     if (req.query != null && req.query.code != null) {
+      debug('handling OAuth callback:', req.query);
+      let successRedirect = options.successRedirect;
+      if (req.query.redirect != null) {
+        successRedirect = req.query.redirect;
+      }
       return passport.authenticate('github', {
-        successRedirect: options.successRedirect || null,
+        successRedirect: successRedirect || null,
         failureRedirect: options.failureRedirect || null
       }).apply(passport, arguments);
     }
 
     // Handle login request.
-    var scope = options.scope;
-    // if (req.query != null && req.query.scope != null) {
-    //   scope = _.chain(req.query.scope.split(','))
-    //     .intersection(['user:email', 'read:org', 'public_repo', 'repo'])
-    //     .union(scope.split(',')).join(',').value();
+    let scope = options.scope;
+    let callbackURL = options.baseURL + (req.originalUrl || req.url);
+    // if (req.query != null) {
+    //   if (req.query.scope != null) {
+    //     scope = _.chain(req.query.scope.split(','))
+    //       .intersection(['user:email', 'read:org', 'public_repo', 'repo'])
+    //       .union(scope.split(',')).join(',').value();
+    //   }
     // }
-    var callbackURL = options.baseURL + (req.originalUrl || req.url);
+    // debug('handling login:', req.query, scope, callbackURL);
     return passport.authenticate('github', {
       scope: scope,
       callbackURL: callbackURL
