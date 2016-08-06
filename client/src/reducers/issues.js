fef22fab398c5df79c21cdd56f3e024a7b6069d7
David Guan <davidguandev@gmail.com>
2016-08-06 12:58:19 +0800

    Fix filter broken working after dragging(#39).

--- client/src/reducers/issues.js
+++ client/src/reducers/issues.js
@@ -14,11 +14,11 @@ export default function issues (state = initialState, action) {
   switch (action.type) {
   case CHANGE_TICKETS:
     state = state.set('tickets',
-      parserTickets(action.payload, state.get('filter')))
+      parserTickets(action.payload, state.get('filter'), true))
     return state
   case CHANGE_FILTER:
     const tickets = state.get('tickets')
-    state = state.set('tickets', parserTickets(tickets, action.payload, true))
+    state = state.set('tickets', parserTickets(tickets, action.payload, false))
     state = state.set('filter', action.payload)
     return state
   default:
