422f1c0ea59873a139481c469e91ef5a611f464a
David Guan <davidguandev@gmail.com>
2016-08-07 22:57:24 +0800

    Using separate db for repos.

--- client/src/helpers/pouchDb.js
+++
@@ -1,7 +0,0 @@
-import PouchDB from 'pouchdb'
-
-// TODO: use seperate repo DB for every repo.
-export const metaDb = new PouchDB('kenhq_meta')
-export const cacheDb = new PouchDB('kenhq_cache')
-
-export default PouchDB
