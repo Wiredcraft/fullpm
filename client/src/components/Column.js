aef9bf92dbb4c2f85a226aef26e751a2c83aba2a
David Guan <davidguandev@gmail.com>
2016-08-16 11:42:16 +0800

    Split IssuesDropTarget out of `Column`

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -4,12 +4,10 @@ import { DragSource, DropTarget } from 'react-dnd'
 
 import Issue from 'components/Issue'
 import 'styles/column'
-import {
-  calcColumnBodyHeight,
-  calcColumnBodyMaxHeight
-} from '../helpers/column'
+import { calcColumnBodyMaxHeight } from '../helpers/column'
 import dropManager from 'helpers/dropManager'
 import { spliceIssueInSync } from 'helpers/tickets'
+import IssuesDropTarget from './IssuesDropTarget'
 
 
 const dragTarget = {
@@ -52,22 +50,18 @@ let intervalId
 export default class Column extends Component {
   constructor() {
     super()
-    this.state = { bodyHeight: 0, bodyMaxHeight: 0, forceUpdater: 0 }
+    this.state = { bodyMaxHeight: 0, forceUpdater: 0 }
   }
 
   componentWillMount() {
     intervalId = setInterval(() => {
       const { id, isOver, onSync } = this.props
-      const { bodyHeight, bodyMaxHeight, forceUpdater } = this.state
+      const { bodyMaxHeight, forceUpdater } = this.state
 
       // Restrict column contained in page without vertical scroll
       const maxHeight = calcColumnBodyMaxHeight(id)
       if (maxHeight !== bodyMaxHeight) {
         this.setState({ bodyMaxHeight: maxHeight })
-      }
-      const height = calcColumnBodyHeight(id)
-      if (height !== bodyHeight) {
-        this.setState({ bodyHeight: height })
       }
 
       if (isOver || onSync) this.setState({ forceUpdater: forceUpdater + 1 })
@@ -81,18 +75,14 @@ export default class Column extends Component {
   render() {
     const {
       connectDragSource,
-      connectDropTarget,
       id,
       isDragging,
-      isOver,
       onSync,
       title
     } = this.props
     let { issues } = this.props
-    const { bodyHeight, bodyMaxHeight } = this.state
-    const { draggingItem, isHoveringIssue } = dropManager
-    const placeHolderAreaHeight =
-      (bodyMaxHeight - bodyHeight) < 0 ? 0 : bodyMaxHeight - bodyHeight
+    const { bodyMaxHeight } = this.state
+    const { draggingItem } = dropManager
 
     const needClone = draggingItem && onSync
     const hasNewItemInSync = needClone && (id === dropManager.newCol)
@@ -106,12 +96,10 @@ export default class Column extends Component {
       if (dropedToNewColumn || dropedToSameColumn) count -= 1
     }
 
-    const appendToTail = isOver && (isHoveringIssue === false)
-
     return connectDragSource(
       <section className='column' id={`column${id}`}>
         <div
-          className='placeholder'
+          className='col-placeholder'
           style={{ display: isDragging ? 'block' : '' }}
         />
         <header className='header'>
@@ -145,20 +133,7 @@ export default class Column extends Component {
             })
           }
           </div>
-          {
-            connectDropTarget(
-              <div
-                className='placeholder-area'
-                style={{ height: placeHolderAreaHeight }}
-              >
-              {
-                ((issues.length === 0 && isOver) || appendToTail) && (
-                  <Issue isPlaceHolder={true} />
-                )
-              }
-              </div>
-            )
-          }
+          <IssuesDropTarget id={id} issues={issues} />
         </div>
       </section>
     )
