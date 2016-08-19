9db7335ed423bbf1b65bb5e4c0a1a2a876c4f5c2
David Guan <davidguandev@gmail.com>
2016-08-19 11:53:45 +0800

    Implement dropdown toggle logic.

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -4,16 +4,49 @@ import '../styles/searchBar'
 
 
 export default class SearchBar extends Component {
+  constructor() {
+    super()
+    this.toggleDropdown = ::this.toggleDropdown
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.toggleDropdown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.toggleDropdown)
+  }
+
+  toggleDropdown(e) {
+    const container = this.refs.container
+    const dropdown = this.refs.dropdown
+    if(!container.contains(e.target)) {
+      dropdown.style.display = 'none'
+    }
+  }
+
+  displayDropdown() {
+    const dom = this.refs.dropdown
+    dom.style.display = 'block'
+  }
+
   render() {
     const { onChange } = this.props
 
     return (
-      <span className='searchBar'>
+      <span
+        ref='container'
+        className='searchBar'
+      >
         <input
           placeholder='Filter issues by title'
           onChange={e => onChange ? onChange(e.target.value) : ''}
+          onClick={() => this.displayDropdown()}
           type='search'
         />
+        <div className='dropdown' ref='dropdown'>
+          dropdown area
+        </div>
       </span>
     )
   }
