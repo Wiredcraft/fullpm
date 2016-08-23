1a690f3f66406ee23dea47c841b501f6197d1f21
David Guan <davidguandev@gmail.com>
2016-08-23 15:53:28 +0800

    Handle popup modal toggle logic.

--- client/src/components/ColumnConfigPopup.js
+++ client/src/components/ColumnConfigPopup.js
@@ -6,7 +6,7 @@ import '../styles/columnConfigPopup'
 export default class ColumnConfigPopup extends Component {
   constructor() {
     super()
-    this.state = { onRenaming: false }
+    this.state = { hidePopup: true }
     this.domClickHandler = ::this.domClickHandler
   }
 
@@ -18,38 +18,55 @@ export default class ColumnConfigPopup extends Component {
     document.removeEventListener('click', this.domClickHandler)
   }
 
-  closePopup() {
-
-  }
-
-  renameHandle() {
-
+  startRename() {
+    setTimeout(() => this.setState({ hidePopup: false }), 5) 
   }
 
   domClickHandler(e) {
-    const container = this.refs.container
+    const { container, modal } = this.refs
     const { closePopup, columnId, switcherClassName } = this.props
-    if(e.target.classList.contains(switcherClassName)) {
-      if(document.querySelector(`#column${columnId}`).contains(e.target)) return
-    }
-    if(!container.contains(e.target)) {
-      closePopup()
+    const { hidePopup } = this.state
+
+    if(hidePopup) {
+      if(e.target.classList.contains(switcherClassName)) {
+        if(document.querySelector(`#column${columnId}`).contains(e.target)) return
+      }
+      if(!container.contains(e.target)) {
+        closePopup()
+      }
+    } else {
+      if(!modal.contains(e.target)) {
+        this.setState({ hidePopup: true })
+      }
     }
   }
 
   render() {
-    const { hide } = this.props
-    const { onRenaming } = this.state
+    const { closePopup, hide } = this.props
+    const { hidePopup } = this.state
 
     return (
+      <div
+        className='column-popup'
+        ref='container'
+        style={{ display: hide ? 'none' : '' }}
+      >
+        <div
+          className='dropdown-switcher'
+          onClick={closePopup}
+        >
+          X
+        </div>
+        <div
+          className='modal-container'
+          style={{ display: hidePopup ? 'none' : 'block' }}
+        >
+          <div className='modal' ref='modal'>
+            Hello world
+          </div>
+        </div>
         <div className='line'>
-        {
-          onRenaming ? (
-            <input type='text' />
-          ) : (
-            <p>Rename</p>
-          )
-        }
+          <p onClick={() => this.startRename()}>Rename</p>
         </div>
         <div className='line'>
           <p>Delete</p>
