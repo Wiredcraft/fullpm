dd3df9ab42976bbbb9aec7c023b0b44335d557c1
Ronan Berder <hunvreus@gmail.com>
2016-08-24 12:08:48 +0800

    Styling some more column headers and board controls

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -141,11 +141,11 @@ export default class Board extends React.Component {
         { notFound && <p>Repo not found</p> }
         <ProgressBar hide={!onLoading} />
         <header className='controls'>
-          <SearchBar onChange={value => changeFilter(value)}/>
+          <SearchBar onChange={value => changeFilter(value)} className='small'/>
           {
             userName && (
               <button
-                className='button primary'
+                className='button primary small'
                 onClick={() => this.newIssue()}
               >
                 New issue
@@ -153,17 +153,8 @@ export default class Board extends React.Component {
             )
           }
           <button
-            className='button'
+            className='button dropdown small'
           >
-            <svg
-              aria-hidden='true'
-              className='icon'
-              height='16'
-              version='1.1'
-              viewBox='0 0 14 16'
-              width='14'>
-                <path d='M14 8.77v-1.6l-1.94-.64-.45-1.09.88-1.84-1.13-1.13-1.81.91-1.09-.45-.69-1.92h-1.6l-.63 1.94-1.11.45-1.84-.88-1.13 1.13.91 1.81-.45 1.09L0 7.23v1.59l1.94.64.45 1.09-.88 1.84 1.13 1.13 1.81-.91 1.09.45.69 1.92h1.59l.63-1.94 1.11-.45 1.84.88 1.13-1.13-.92-1.81.47-1.09L14 8.75v.02zM7 11c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z'></path>
-            </svg>
             Settings
           </button>
           <a
